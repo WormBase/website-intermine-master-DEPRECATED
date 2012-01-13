233a5363f5e59cbf453de65f991169e4a10835e7
Todd Harris <info@toddharris.net>
2012-01-13 22:59:21 +0000

    hack script for fetching and processing core genomic annotations

--- bin/get_genomic_annotations.pl
+++ bin/get_genomic_annotations.pl
@@ -3,18 +3,38 @@
 # Quick data staging script for building a new intermine release.
 
 WSVERSION=$1
-DATAROOT=/usr/local/wormbase/intermine/data/${WSVERSION}/
 
+if [ ! ${WSVERSION} ]; then
+  echo "Usage: $0 [WSVERSION]"
+  exit
+fi
+
+DATAROOT=/usr/local/wormbase/intermine/data/${WSVERSION}/
+#        VERSION    /  SOURCE / SPECIES  / TYPE
 mkdir -p ${DATAROOT}/wormbase/c_elegans/gff3
 mkdir -p ${DATAROOT}/wormbase/c_elegans/fasta
 
-# C. elegans genomic annotations
-cd ${DATAROOT}/wormbase/c_elegans/gff3
-# curl 
-# curl -O ftp://www.wormbase.org/pub/wormbase/releases/${WSVERSION}/species/c_elegans/c_elegans.${WSVERSION}.annotations.gff3.gz
-# Or via CP
-cp /usr/local/ftp/pub/wormbase/releases/${WSVERSION}/species/c_elegans/c_elegans.${WSVERSION}.annotations.gff3.gz c_elegans.current.annotations.gff3.gz
+ALL_SPECIES=(c_elegans);
 
-# C. elegans fasta sequence
-cp /usr/local/ftp/pub/wormbase/releases/${WSVERSION}/species/c_elegans/c_elegans.${WSVERSION}.annotations.gff3.gz c_elegans.current.genomic.fa.gz
+for SPECIES in ${ALL_SPECIES}; do
 
+    # genomic annotations
+    cd ${DATAROOT}/wormbase/${SPECIES}/gff3
+    curl -O ftp://ftp.wormbase.org/pub/wormbase/releases/${WSVERSION}/species/${SPECIES}/${SPECIES}.${WSVERSION}.annotations.gff3.gz
+    mv ${SPECIES}.${WSVERSION}.annotations.gff3.gz ${SPECIES}.current.annotations.gff3.gz
+    # cp /usr/local/ftp/pub/wormbase/releases/${WSVERSION}/species/c_elegans/c_elegans.${WSVERSION}.annotations.gff3.gz c_elegans.current.annotations.gff3.gz
+
+    # FASTA sequence
+    cd ${DATAROOT}/wormbase/${SPECIES}/fasta
+    curl -O ftp://ftp.wormbase.org/pub/wormbase/releases/${WSVERSION}/species/${SPECIES}/${SPECIES}.${WSVERSION}.genomic.fa.gz 
+    mv ${SPECIES}.${WSVERSION}.genomic.fa.gz ${SPECIES}.current.genomic.fa.gz
+    #cp /usr/local/ftp/pub/wormbase/releases/${WSVERSION}/species/c_elegans/c_elegans.${WSVERSION}.annotations.gff3.gz c_elegans.current.genomic.fa.gz
+    gunzip ${SPECIES}.current.genomic.fa.gz
+
+    # Update the root-level symlink
+    cd ${DATAROOT}
+    rm -rf current
+    ln -s ${VERSION} current
+   
+    # Intermine can't load a concatenated fasta?
+done   
